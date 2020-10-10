a590960df9d5d695327aecac8ef42e500b17f30d
Joe Zawisa <jazawisa@umich.edu>
2020-10-09 22:32:24 -0400

    Begin Xcode project

---
+++ frontend/music-sharing-network/music-sharing-network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music-sharing-network
+//
+//  Created by Joe Zawisa on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
