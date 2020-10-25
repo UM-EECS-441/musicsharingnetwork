2728c92346c17ea5af695885d243a921675985de
Joe Zawisa <jazawisa@umich.edu>
2020-10-24 21:28:27 -0400

    Add a few UI elements

--- frontend/music-sharing-network/music-sharing-network/ViewController.swift
+++ frontend/music-sharing-network/music-sharing-network/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK:- TableView handlers
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // how many sections are in table
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // how many rows per section
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // event handler when a cell is tapped
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // populate a single cell
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableCell", for: indexPath)
+        
+        return cell
+    }
 }
 
