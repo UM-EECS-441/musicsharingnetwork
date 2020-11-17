4e1e07c0e38b734772e5aee93f753ee546df4862
Austin Peash <apeash@umich.edu>
2020-11-17 12:42:45 -0500

    Made follow/unfollow API call but get 404 error

--- frontend/music-sharing-network/music-sharing-network/TimelineTableCell.swift
+++ frontend/music-sharing-network/music-sharing-network/TimelineTableCell.swift
@@ -34,45 +34,76 @@ class TimelineTableCell: UITableViewCell {
         self.isFollowed.toggle()
         
         if isFollowed {
-            followButton.setTitle("Unfollow", for: [])
-            // API request to unfollow user
             
-            /* HTTP Request [API endpoint needs to be made]
-            // Serialize the username and password into JSON data
-            let json: [String: Any] = ["post_id": identifier]
-            let jsonData = try? JSONSerialization.data(withJSONObject: json)
+            // API request to follow user
+            //let json: [String: Any] = ["username": targetUser]
+            //let jsonData = try? JSONSerialization.data(withJSONObject: json)
             
             // Build an HTTP request
-            let requestURL = SharedData.baseURL + "/posts/" + identifier! + "/like/"
+            let targetUser: String = self.usernameLabel.text ?? "ERROR"
+            let requestURL = SharedData.baseURL + "/users/" + targetUser + "/follow/"
             var request = URLRequest(url: URL(string: requestURL)!)
             request.httpShouldHandleCookies = true
             request.httpMethod = "POST"
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = jsonData
+            //request.httpBody = jsonData
             
             // Send the request and read the server's response
             SharedData.SynchronousHTTPRequest(request) { (data, response, error) in
                 // Check for errors
                 guard let _ = data, error == nil else {
-                    print("TimelineTableCellVC > likeTapped: NETWORKING ERROR")
+                    print("TimelineTableCellVC > followTapped: NETWORKING ERROR")
                     return
                 }
                 
                 if let httpResponse = response as? HTTPURLResponse {
                     // Check for errors
                     if httpResponse.statusCode != 201 {
-                        print("TimelineTableCellVC > likeTapped: HTTP STATUS: \(httpResponse.statusCode)")
+                        print("TimelineTableCellVC > followTapped: HTTP STATUS: \(httpResponse.statusCode)")
                         return
                     }
                     
                 }
             }
-             */
+            
+            followButton.setTitle("Unfollow", for: [])
             
         } else {
-            followButton.setTitle("Follow", for: [])
+            
             // API request to follow user
+            //let json: [String: Any] = ["username": targetUser]
+            //let jsonData = try? JSONSerialization.data(withJSONObject: json)
+            
+            // Build an HTTP request
+            let targetUser: String = self.usernameLabel.text ?? "ERROR"
+            let requestURL = SharedData.baseURL + "/users/" + targetUser + "/unfollow/"
+            var request = URLRequest(url: URL(string: requestURL)!)
+            request.httpShouldHandleCookies = true
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            //request.httpBody = jsonData
+            
+            // Send the request and read the server's response
+            SharedData.SynchronousHTTPRequest(request) { (data, response, error) in
+                // Check for errors
+                guard let _ = data, error == nil else {
+                    print("TimelineTableCellVC > followTapped: NETWORKING ERROR")
+                    return
+                }
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    // Check for errors
+                    if httpResponse.statusCode != 201 {
+                        print("TimelineTableCellVC > followTapped: HTTP STATUS: \(httpResponse.statusCode)")
+                        return
+                    }
+                    
+                }
+            }
+            
+            followButton.setTitle("Follow", for: [])
             
         }
     }
