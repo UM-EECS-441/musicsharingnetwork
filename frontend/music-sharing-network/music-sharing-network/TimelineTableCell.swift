ac35d157693b8e8cbe075035454e8dad0fefd04f
Austin Peash <apeash@umich.edu>
2020-11-17 12:12:50 -0500

    Can toggle Follow button

--- frontend/music-sharing-network/music-sharing-network/TimelineTableCell.swift
+++ frontend/music-sharing-network/music-sharing-network/TimelineTableCell.swift
@@ -17,14 +17,64 @@ class TimelineTableCell: UITableViewCell {
     @IBOutlet weak var textBox: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var followButton: UIButton!
     
     var likeButtonAction : (() -> Void)?
     var isLiked: Bool = false
+    var isFollowed: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.likeButton.addTarget(self, action: #selector(likeTapped(_:)), for: .touchUpInside)
+    }
+    
+    @IBAction func followTapped(_ sender: Any) {
+        
+        self.isFollowed.toggle()
+        
+        if isFollowed {
+            followButton.setTitle("Unfollow", for: [])
+            // API request to unfollow user
+            
+            /* HTTP Request [API endpoint needs to be made]
+            // Serialize the username and password into JSON data
+            let json: [String: Any] = ["post_id": identifier]
+            let jsonData = try? JSONSerialization.data(withJSONObject: json)
+            
+            // Build an HTTP request
+            let requestURL = SharedData.baseURL + "/posts/" + identifier! + "/like/"
+            var request = URLRequest(url: URL(string: requestURL)!)
+            request.httpShouldHandleCookies = true
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonData
+            
+            // Send the request and read the server's response
+            SharedData.SynchronousHTTPRequest(request) { (data, response, error) in
+                // Check for errors
+                guard let _ = data, error == nil else {
+                    print("TimelineTableCellVC > likeTapped: NETWORKING ERROR")
+                    return
+                }
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    // Check for errors
+                    if httpResponse.statusCode != 201 {
+                        print("TimelineTableCellVC > likeTapped: HTTP STATUS: \(httpResponse.statusCode)")
+                        return
+                    }
+                    
+                }
+            }
+             */
+            
+        } else {
+            followButton.setTitle("Follow", for: [])
+            // API request to follow user
+            
+        }
     }
     
     @IBAction func likeTapped(_ sender: Any) {
